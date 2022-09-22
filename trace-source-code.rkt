c46b5017e344852aaad3c2d750952ee7af719e9f
Jos Koot <jos.koot@gmail.com>
2022-09-22 02:41:05 +0200

    Update trace-source-code.rkt

--- trace-source-code.rkt
+++ trace-source-code.rkt
@@ -4,7 +4,7 @@
 
 ; WARNING
 ;
-; Running this module produces more than 100000 lines of output.
+; Running this module produces more than 2000 lines of output.
 ; Maximum line length 90.
 
 (if (eq? (system-type 'vm) 'racket)
